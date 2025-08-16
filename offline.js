17c29d098361d9c35e608efebba9deb334d80428
Kara Cannon <hello@kararyli.net>
2025-08-16 12:05:38 -0700

    Add PWA details

---
+++ offline.js
@@ -0,0 +1,87 @@
+const cacheVersion = 1;
+const cachePrefix = 'tog-';
+const files = [
+  './apple-touch-icon.html',
+  './apple-touch-icon.png',
+  './fonts/HankenGrotesk/HankenGrotesk-Italic-VariableFont_wght.ttf',
+  './fonts/HankenGrotesk/HankenGrotesk-VariableFont_wght.ttf',
+  './fonts/TheNautigal/TheNautigal-Bold.ttf',
+  './fonts/TheNautigal/TheNautigal-Regular.ttf',
+  './icon.png',
+  './',
+  './index.html',
+  './offline.js',
+  './tog.css',
+  './tog.js'
+];
+const cacheName = `${cachePrefix}${cacheVersion}`;
+
+async function cacheFiles() {
+  // Download new files
+  const cacheStorage = await caches.open(cacheName);
+  await Promise.allSettled(
+    files.map((file) => cacheStorage.add(file).then(() => {
+      console.info(`Cache success for file ${file}`);
+    }, (error) => {
+      console.error(`Cache failed for file ${file}`, error);
+      throw error;
+    }))
+  );
+
+  // Delete any old caches
+  const keys = await caches.keys();
+  for (const key of keys) {
+    const isOurCache = key.startsWith(cachePrefix);
+    if (isOurCache && !key == cacheName) {
+      console.info(`deleting cache ${key}`);
+      caches.delete(key);
+    }
+  }
+}
+
+function fetchLive(request) {
+  return fetch(request).then((result) => {
+    console.info('received live result', request.url);
+    return result;
+  }, (error) => {
+    console.info('live fetch failed', request.url, error);
+  });
+}
+
+function raceWithCached(request, cached) {
+  const timeout = 300;
+  return Promise.race(
+    fetchLive(request),
+    new Promise(
+      (res) => setTimeout(
+        () => {
+          console.info('using cached backup', request.url)
+          res(cached);
+        }
+      ), timeout)
+  );
+}
+
+async function fallBackToCached(request) {
+  let cachedResponse;
+  // try to fetch from cache
+  try {
+    const cache = await caches.open(cacheName);
+    cachedResponse = await cache.match(request);
+    console.info('cache hit', request.url);
+  } catch(cacheError) {
+    console.info('cache miss', request.url, cacheError);
+  }
+  // if we have cached response, don't wait long for
+  // live data
+  return cachedResponse ?
+    raceWithCached(request, cachedResponse) :
+    fetchLive(request);
+}
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(cacheFiles());
+});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fallBackToCached(event.request));
+});
---
+++ tog.js
@@ -0,0 +1,57 @@
+function toggle(li, value) {
+  li.querySelector('input').checked = value;
+}
+
+function loadState() {
+  const checkboxes = document.body.querySelectorAll('input[type=checkbox]');
+  localStorage.tog?.split(',').forEach((value, index) => {
+    checkboxes.item(index).checked = !!value;
+  });
+}
+
+function saveState() {
+  const checkboxes = document.body.querySelectorAll('input[type=checkbox]');
+  localStorage.tog = Array.from(checkboxes).map((el) => el.checked ? '1' : '');
+}
+
+document.querySelector('.chapter-list').addEventListener('change', (e) => {
+  let i = e.target;
+  do {
+    i = i.parentNode;
+  } while (i?.tagName !== 'LI');
+  const targetClassName = i.className;
+
+  // if we're checking, check all previous chapters in the book
+  if (e.target.checked) {    
+    while (i.previousElementSibling) {
+      if (i.previousElementSibling?.className == targetClassName) {
+        toggle(i.previousElementSibling, true);
+      }
+      i = i.previousElementSibling;
+    }
+  }
+  else {
+    while (i.nextElementSibling) {
+      if (i.nextElementSibling.className == targetClassName) {
+        toggle(i.nextElementSibling, false);
+      }
+      i = i.nextElementSibling;
+    }
+  }
+
+  // sync state
+  saveState();
+});
+
+document.querySelector('.book-list').addEventListener('change', () => saveState());
+
+// save after load in case we change the serialization
+loadState();
+saveState();
+
+navigator?.serviceWorker.register('./offline.js')
+  .then((registration) => {
+    console.log('Service Worker Registered', registration);
+  }, (error) => {
+    console.log('Service Worker Registration failed', error);
+  });
